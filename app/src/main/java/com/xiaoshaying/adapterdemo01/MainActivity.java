d76ee188c88949e94d75810de5f7e9508db5d93e
AlongEagle <1107635271@qq.com>
2015-12-05 18:14:39 +0800

    code someting

--- app/src/main/java/com/xiaoshaying/adapterdemo01/MainActivity.java
+++ app/src/main/java/com/xiaoshaying/adapterdemo01/MainActivity.java
@@ -2,12 +2,24 @@ package com.xiaoshaying.adapterdemo01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayAdapter<String> arrayAdapter;
+    private SimpleAdapter simpleAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView= (ListView) findViewById(R.id.listView);
+        String [] arry_date={"项目1","项目3","项目4","项目1","项目1",};
+
+        arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arry_date);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
